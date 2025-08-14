b16500bdc3588dfae806f83ae1e51fcbbbfaa822
Gabriel Martins <gabrielmts33@gmail.com>
2025-08-13 22:20:40 -0300

    Add files via upload

---
+++ Projetos em C#/exercicios para exercitar.cs
@@ -0,0 +1,143 @@
+﻿
+
+
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+//Lógica Básica
+
+
+//double soma;
+//int numero1; 
+//int numero2;
+//Console.Write(" digite um numero: ");
+//numero1  = Convert.ToInt32(Console.ReadLine()); 
+
+//Console.Write(" digite outro numero: ");
+//numero2  = Convert.ToInt32 (Console.ReadLine());
+
+//soma = numero1 + numero2;
+
+//Console.Write($" o resultado da soma é :  {soma} ");
+
+
+//Estrutura Condicional
+
+//int idade = 0; 
+
+//Console.WriteLine(" escreva sua idade"); 
+//idade = Convert.ToInt32( Console.ReadLine() );
+
+//if (idade >= 18)
+//{
+//    Console.WriteLine(" MAIOR DE IDADE ");
+//} 
+//  else
+//{
+//    Console.WriteLine(" MENOR DE IDADE "); 
+
+
+// Estrutura Escolha-Caso
+
+//Console.WriteLine("LISTA DE PRODUTOS ");
+//Console.WriteLine("escolha o produto para consultar o preço");
+//Console.WriteLine("1 - arroz  ");
+//Console.WriteLine("2 - feijao  ");
+//Console.WriteLine("3 - macarrão");
+//Console.WriteLine("4 - molho de tomate  ");
+//Console.WriteLine("5 - bife de figado ");
+
+//int opcao = Convert .ToInt32(Console.ReadLine());
+
+//switch (opcao)
+//{
+//    case 1:
+//        Console.WriteLine(" arroz 5kg: R$25,00");
+//        break;
+//    case 2:
+//        Console.WriteLine(" feijao 1kg: R$ 19,00");
+//        break;
+
+//    case 3:
+//        Console.WriteLine(" macarrão 500mg: R$15,00");
+//        break;
+
+//    case 4:
+//        Console.WriteLine(" molho de tomate: R$6,00");
+//        break;
+
+//    case 5:
+//        Console.WriteLine(" bife de figado: R$ 35,00");
+//        break;
+
+//    default: 
+//        Console.WriteLine(" opcao invalida"); 
+//        break;
+//}
+
+//(enquanto / enquanto - faca)
+
+//string resposta;
+
+//do
+//{
+//    Console.WriteLine("LISTA DE PRODUTOS");
+//    Console.WriteLine("Escolha o produto para consultar o preço.");
+//    Console.WriteLine("1 - Arroz");
+//    Console.WriteLine("2 - Feijão");
+//    Console.WriteLine("3 - Macarrão");
+//    Console.WriteLine("4 - Molho de tomate");
+//    Console.WriteLine("5 - Bife de fígado");
+//    int opcao = Convert.ToInt32(Console.ReadLine());
+
+//    switch (opcao)
+//    {
+//        case 1:
+//            Console.WriteLine("Arroz 5kg: R$25,98");
+//            break;
+//        case 2:
+//            Console.WriteLine("Feijão 1kg: R$15,90");
+//            break;
+//        case 3:
+//            Console.WriteLine("Macarrão 500g - R$3,49");
+//            break;
+//        case 4:
+//            Console.WriteLine("Molho de tomate 250g - R$2,98");
+//            break;
+//        case 5:
+//            Console.WriteLine("Bife de fígado kg - R$18,90");
+//            break;
+//        default:
+//            Console.WriteLine("Opção inválida!");
+//            break;
+//    }
+//    Console.WriteLine("Deseja fazer uma nova consulta? [S/N]");
+//    resposta = Console.ReadLine();
+//    Console.Clear();
+//} while (resposta == "S");
+
+
+//for (int cont = 1; cont <= 20; cont++)
+
+//    Console.WriteLine(cont); 
+
+
+//int[] numeros = new int[5];
+
+//for (int i = 0; i < numeros.Length; i++) ;
+
+//{
+
+//    Console.WriteLine(" digite um numero ");
+//    numeros[1] = Convert.ToInt32(Console.ReadLine());
+
+//}
+
+//for (int i = 0; i < numeros.Length; i++) 
+//{
+//    Console.WriteLine(numeros[i]); 
+//}
+
+
+
+
