884006ecf1cb18c0a810933d2beca153d084ba2f
Gabriel Martins <gabrielmts33@gmail.com>
2025-08-13 22:21:33 -0300

    Add files via upload

---
+++ Projetos em C#/estrutura (faça- enquanto) - C#.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+int senha = 777;
+
+
+do
+
+{
+    Console.WriteLine("digite sua senha: ");
+    senha = Convert.ToInt32(Console.ReadLine());
+
+    if (senha == 777)
+    {
+        Console.WriteLine(" sua senha esta correta ");
+
+} 
+
+
+} while (senha != 777);
+
+
+
+   
+
+
+  
