da1d5c6b30c27d8e3dae549c143180132fc3217d
Gabriel Martins <gabrielmts33@gmail.com>
2025-08-13 22:16:37 -0300

    Add files via upload

---
+++ Projetos em C#/Livro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public int Ano; 
+
+
+        public void ExibirInformacoes()
+        {
+          
+            Console.WriteLine($"{Titulo} - {Autor} - ({Ano})");
+
+ }
+
+        public void Atualizacao (int novoAno)
+        {
+            Ano = novoAno;
+        }
+
+        public int CalcularIdade()
+        {
+            return DateTime .Now.Year - Ano;
+        }
+    }
+
+}
